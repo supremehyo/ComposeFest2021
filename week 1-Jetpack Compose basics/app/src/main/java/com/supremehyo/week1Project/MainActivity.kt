0d287c0ae1bcdbcebf840776916794f2952c596d
supremehyo <supreme_hyo@naver.com>
2021-11-04 21:09:35 +0900

    1주차 완료
    LazyColumn ,  animateDpAsState , rememberSaveable 에 대해 학습

--- week 1-Jetpack Compose basics/app/src/main/java/com/supremehyo/week1Project/MainActivity.kt
+++ week 1-Jetpack Compose basics/app/src/main/java/com/supremehyo/week1Project/MainActivity.kt
@@ -3,17 +3,21 @@ package com.supremehyo.week1Project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +30,8 @@ class MainActivity : ComponentActivity() {
             Week1JetpackComposeBasics2Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MyProject()
+                   // MyProject()
+                    Greetings()
                 }
             }
         }
@@ -48,30 +53,53 @@ private fun MyProject(names : List<String> = listOf("supreme" , "Hyo")) {
 }
 
 @Composable
+private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+    //recycler랑 비슷하다 훨씬 간편하게 레이아웃을 구성하게 해준다.
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
     //var expanded = false 컴포저블 내부의 변수에 할당할 수는 없다.
     //재구성이 일어나면 값이 새상태로 재설정 될 수 있다.
     // 재구성 상태에서 값을 유지하려면 remember를 사용해야한다.
-    val expanded = remember { mutableStateOf(false) }
+   // val expanded = remember { mutableStateOf(false) }
+
+    //rememberSaveable 은 화면이 돌아가거나 다크모드로 변경되는 상황에도 값을 유지한다.
+    var expanded by remember { mutableStateOf(false) }
     //펼치기 접기할때 변경될 padding 의 값 48dp가 됐다가 expanded 변하면 0dp가 된다.
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    //val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    //애니메이션 효과 추가하기
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
     Surface(
         color = MaterialTheme.colors.primary
         ,modifier = Modifier.padding(vertical = 4.dp , horizontal = 8.dp))
     {
         Row(modifier =  Modifier.padding(12.dp)){
             Column(modifier = Modifier.weight(1f)
-                .padding(bottom = extraPadding)) //여기서 extraPadding이 쓰이면서 재구성할때
-            //패딩값이 변하게 된다.
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) //여기서 extraPadding이 쓰이면서 재구성할때
+            //패딩값이 변하게 된다.  coerceAtLeast은 패딩이 0dp 이하로 가지 않게 제약을 주는것
             {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name , style = MaterialTheme.typography.h4)
             }
-            OutlinedButton(onClick = {
-                expanded.value = !expanded.value
-            })
+            OutlinedButton(
+                onClick = { expanded = !expanded }
+            )
             {
-                Text(if (expanded.value)  "접기" else "펼치기")
+                Text(if (expanded)  "접기" else "펼치기")
             }
         }
 
@@ -82,6 +110,7 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     Week1JetpackComposeBasics2Theme {
-        MyProject()
+        Greetings()
+     //   MyProject()
     }
 }
